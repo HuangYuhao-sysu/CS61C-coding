3cbfc96c813a996f93dfd18845055e47f249436e
HuangYuhao-sysu <1823551220@qq.com>
2023-10-29 13:20:07 +0800

    [CS61C]Finish Part A2.

--- proj1/steganography.c
+++ proj1/steganography.c
@@ -21,13 +21,32 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+    Color *returnColor = (Color *) malloc(sizeof (Color));
+    returnColor->R = (image->image)[row][col].B & 0x01 ? 0xff : 0x00;
+    returnColor->G = (image->image)[row][col].B & 0x01 ? 0xff : 0x00;
+    returnColor->B = (image->image)[row][col].B & 0x01 ? 0xff : 0x00;
+    return returnColor;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+	// New an image
+    Image *returnImage = (Image *) malloc(sizeof (Image));
+    returnImage->rows = image->rows;
+    returnImage->cols = image->cols;
+    returnImage->image = (Color **) malloc(sizeof (Color *) * returnImage->rows);
+    for (int i = 0; i < returnImage->rows; ++i) {
+        (returnImage->image)[i] = (Color *) malloc(sizeof (Color) * returnImage->cols);
+    }
+    for (int i = 0; i < returnImage->rows; ++i) {
+        for (int j = 0; j < returnImage->cols; ++j) {
+            Color *returnColor = evaluateOnePixel(image, i, j);
+            (returnImage->image)[i][j] = *returnColor;
+            free(returnColor);
+        }
+    }
+    return returnImage;
 }
 
 /*
@@ -43,7 +62,24 @@ If the input is not correct, a malloc fails, or any other error occurs, you shou
 Otherwise, you should return from main with code 0.
 Make sure to free all memory before returning!
 */
+void processCLI(int argc, char **argv, char **filename)
+{
+    if (argc != 2) {
+        printf("usage: %s filename\n",argv[0]);
+        printf("filename is an ASCII PPM file (type P3) with maximum value 255.\n");
+        exit(-1);
+    }
+    *filename = argv[1];
+}
+
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+	Image *image, *hidden;
+    char *filename;
+    processCLI(argc, argv, &filename);
+    image = readData(filename);
+    hidden = steganography(image);
+    writeData(hidden);
+    freeImage(image);
+    freeImage(hidden);
 }
